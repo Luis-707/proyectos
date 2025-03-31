145253aa49246a8adcee4c655687a3f86145be15
Luis-707 <lgalanton4@gmail.com>
2025-03-31 18:53:48 -0400

    Comentado y explicado

--- P1/Proyectos/script_2.js
+++ P1/Proyectos/script_2.js
@@ -20,9 +20,12 @@ anagrama(); // Llamada de prueba */
 /* Modo 2 */
 
 function anagrama() {
+
+    //Declaramos las variables dentro de la funcion
     let palabra1 = "Lacteo";
     let palabra2 = "coleta";
 
+    //Se imprimen y visualizan las palabras que se compararan,ademas de decir si son o no anagramas
     console.log("Palabras ingresadas:", palabra1, "y", palabra2);
     console.log("¿Son anagramas?", anagram(palabra1, palabra2));
 }
@@ -31,6 +34,10 @@ function anagram(wordOne, wordTwo) {
     if (wordOne.toLowerCase() === wordTwo.toLowerCase()) {
         return false;
     }
+
+    //Primero pasamos las palabras a minusculas,
+    // las convertimos en un array de caracteres 
+    // para luego ordenarlas alfabeticamente y de ahi compararlas a ver si tienen o no parecido
     return wordOne.toLowerCase().split("").sort().join("") === wordTwo.toLowerCase().split("").sort().join("");
 }
 
