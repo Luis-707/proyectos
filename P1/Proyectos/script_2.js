b25d6d6ff36d5a10b7ac8d77f78397ba206c7d60
Luis-707 <lgalanton4@gmail.com>
2025-03-31 18:36:02 -0400

    Palabra enunciada

--- P1/Proyectos/script_2.js
+++ P1/Proyectos/script_2.js
@@ -4,9 +4,12 @@ function anagrama() {
 
 function anagram(wordOne, wordTwo) {
     if (wordOne.toLowerCase() === wordTwo.toLowerCase()) {
-        return false;
 
         console.log("Palabra que no es anagrama: ", wordOne);
+
+        return false;
+
+       
    
     }
     return wordOne.toLowerCase().split("").sort().join("") === wordTwo.toLowerCase().split("").sort().join("");
