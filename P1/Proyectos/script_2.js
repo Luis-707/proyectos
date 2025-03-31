e0a81bdc45f5c0c78885b4a3e3e5555b740606ae
Luis-707 <lgalanton4@gmail.com>
2025-03-31 18:34:51 -0400

    CA_3

--- P1/Proyectos/script_2.js
+++ P1/Proyectos/script_2.js
@@ -1,10 +1,13 @@
 function anagrama() {
-    console.log(anagram("amor", "roma"));
+    console.log(anagram("Lacteo", "coleta"));
 }
 
 function anagram(wordOne, wordTwo) {
     if (wordOne.toLowerCase() === wordTwo.toLowerCase()) {
         return false;
+
+        console.log("Palabra que no es anagrama: ", wordOne);
+   
     }
     return wordOne.toLowerCase().split("").sort().join("") === wordTwo.toLowerCase().split("").sort().join("");
 }
