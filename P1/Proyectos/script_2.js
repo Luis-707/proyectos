e806bca1ed2280004d1e2e2be2b2006477ba2fa9
Luis-707 <lgalanton4@gmail.com>
2025-03-31 18:48:11 -0400

    agregado del modo 2

--- P1/Proyectos/script_2.js
+++ P1/Proyectos/script_2.js
@@ -1,4 +1,6 @@
-function anagrama() {
+/*Modo 1*/
+
+/* function anagrama() {
     console.log(anagram("Lacteo", "coleta"));
 }
 
@@ -13,4 +15,24 @@ function anagram(wordOne, wordTwo) {
     return wordOne.toLowerCase().split("").sort().join("") === wordTwo.toLowerCase().split("").sort().join("");
 }
 
+anagrama(); // Llamada de prueba */
+
+/* Modo 2 */
+
+function anagrama() {
+    let palabra1 = "Lacteo";
+    let palabra2 = "coleta";
+
+    console.log("Palabras ingresadas:", palabra1, "y", palabra2);
+    console.log("¿Son anagramas?", anagram(palabra1, palabra2));
+}
+
+function anagram(wordOne, wordTwo) {
+    if (wordOne.toLowerCase() === wordTwo.toLowerCase()) {
+        return false;
+    }
+    return wordOne.toLowerCase().split("").sort().join("") === wordTwo.toLowerCase().split("").sort().join("");
+}
+
 anagrama(); // Llamada de prueba
+
