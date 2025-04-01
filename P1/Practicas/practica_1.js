e56c88bf1f49a622fee3610ae5227729486e4623
Luis-707 <lgalanton4@gmail.com>
2025-04-01 17:31:44 -0400

    practica 1 lista

---
+++ P1/Practicas/practica_1.js
@@ -0,0 +1,34 @@
+function multiplicacion(a,b){
+
+return(a * b);
+
+}
+
+//otra parte
+
+let nro1 = 12;
+
+let nro2 = 4;
+
+
+console.log("El resultado es: ", multiplicacion(nro1,nro2));
+
+
+let nroa = 50;
+
+let nrob = 40;
+
+var resultado_suma = nroa + nrob;
+
+var dato_adicional = resultado_suma * 50;
+
+
+if (resultado_suma >= 100) {
+
+    console.log("El resultado otro resultado es: ", dato_adicional);
+    
+} else {
+
+    console.log("El resultado otro resultado es: ", resultado_suma);
+    
+}
